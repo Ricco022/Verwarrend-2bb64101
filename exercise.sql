6b200706fde95d307c96530fd6cdfa3acb3b1f3d
Riccardo van der Horst <riccardo271002@gmail.com>
2020-02-06 12:20:16 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,1 @@
+UPDATE sterrenstelsel.planeten t SET t.naam = 'Teenalp' WHERE t.naam LIKE 'Mars' ESCAPE '#' AND t.diameter = 6794 AND t.`Afstand tot de zon` = 227936640 AND t.`massa` = 0 AND t.bezoek_datum IS NULL AND t.id = 6
